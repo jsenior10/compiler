2c1fc500b9efcdc6e14bd8c58aab0537e2bd3709
Jacob Senior <32003487+jsenior10@users.noreply.github.com>
2024-10-12 21:21:45 +0100

    Added CallStatement class

--- compiler/Statement.h
+++ compiler/Statement.h
@@ -16,3 +16,18 @@ private:
 };
 
 using StatementPtr = std::unique_ptr<Statement>;
+
+class CallStatement : public Statement
+{
+public:
+	CallStatement(CallExpressionPtr&& callExp)
+		: m_callExp(std::move(callExp))
+	{}
+	
+	const CallExpression& GetCallExpression() const { return *m_callExp; }
+
+	void Dispatch(StatementVisitor& visitor) override { visitor.Visit(*this); }
+
+private:
+	CallExpressionPtr m_callExp;
+};
