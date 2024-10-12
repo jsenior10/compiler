b7ee57a9df03dccce9512e65637bb4d3ccd37d24
Jacob Senior <32003487+jsenior10@users.noreply.github.com>
2024-10-12 21:29:07 +0100

    Added AssignState,ent class

--- compiler/Statement.h
+++ compiler/Statement.h
@@ -31,3 +31,27 @@ public:
 private:
 	CallExpressionPtr m_callExp;
 };
+
+class AssignStatement : public Statement
+{
+public:
+	AssignStatement(const std::string& varName, ExpressionPtr&& rval)
+		: m_varName(varName)
+		, m_rval(std::move(rval))
+	{}
+
+	const std::string& GetVarName() const { return m_varName; }
+
+	const Expression& getRval() const { return *m_rval; }
+
+	const VariableDeclaration* GetVarDecl() const { return m_varDecl; }
+
+	void SetVarDecl(const VariableDeclaration* varDecl) { m_varDecl = varDecl; }
+
+	void Dispatch(StatementVisitor& visitor) override { visitor.Visit(*this); }
+
+private:
+	std::string m_varName;
+	ExpressionPtr m_rval;
+	const VariableDeclaration* m_varDecl;
+};
