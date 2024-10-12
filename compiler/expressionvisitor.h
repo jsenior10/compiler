0be03458e2cf3ca19152851f083627b61877dc58
Jacob Senior <32003487+jsenior10@users.noreply.github.com>
2024-10-12 20:24:56 +0100

    Added ExpressionVisitor and StatementVisitor classes

---
+++ compiler/expressionvisitor.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "syntax.h"
+
+class ExpressionVisitor
+{
+public:
+	virtual void* Visit(BoolExpression& expression) = 0;
+	virtual void* Visit(IntExpression& expression) = 0;
+	virtual void* Visit(VariableExpression& expression) = 0;
+	virtual void* Visit(CallExpression& expression) = 0;
+};
+
+class StatementVisitor
+{
+public:
+	virtual void* Visit(CallStatement& expression) = 0;
+	virtual void* Visit(AssignStatement& expression) = 0;
+	virtual void* Visit(ReturnStatement& expression) = 0;
+	virtual void* Visit(IfStatement& expression) = 0;
+	virtual void* Visit(WhileStatement& expression) = 0;
+};
